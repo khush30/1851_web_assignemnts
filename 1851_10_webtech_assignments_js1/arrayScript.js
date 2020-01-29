0bdadc7ab3bbfcc026e4409faf7bb2ef8885acab
khush30 <55485684+khush30@users.noreply.github.com>
2020-01-29 02:03:32 -0800

    sorting added

--- 1851_10_webtech_assignments_js1/arrayScript.js
+++ 1851_10_webtech_assignments_js1/arrayScript.js
@@ -23,3 +23,16 @@ function maxFunc(arr)
             max = arr[i];
     return max;
 }
+
+function sortFunc(arr)
+{
+    for( i = 1 ; i < arr.length ; i++ )
+		for(j = 0 ; j < arr.length-i ; j++ )
+			if(arr[j] > arr[j+1])
+			{
+				arr[j] = arr[j] + arr[j+1];
+				arr[j+1] = arr[j] - arr[j+1];
+				arr[j] = arr[j] - arr[j+1];
+			}
+	return arr;
+}
